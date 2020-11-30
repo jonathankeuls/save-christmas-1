a7e27ce31acb777f28c702ea2ad47d9fb8aa1096
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-30 15:07:02 +0100

    Steg 2

--- statistics.js
+++ statistics.js
@@ -1,12 +1,24 @@
 
-function max(arr) {
+const array = [3, 4, 8, 0, 6, 7, 1, 0, 10, 11, 45, 9, 16]
 
+function max(arr) {
+    return Math.max.apply(Math, arr)
 }
+console.log(max(array))
+
 
 function min(arr) {
-
+    return Math.min.apply(Math, arr)
 }
+console.log(min(array))
 
 function mean(arr) {
     // Ledtråd: Börja med att räkna ut summan av alla element
+    let sum = 0
+    for (i = 0; i < arr.length; i++) {
+      sum = sum + arr[i]
+    }
+    return sum/arr.length
 }
+console.log(mean(array))
+console.log(mean([1, 2, 6, 8]))
