52feb0f87ace387c17a3d078d2d0f43dfbefdec5
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-12-02 12:10:43 +0100

    Steg 4

--- filter.js
+++ filter.js
@@ -9,6 +9,15 @@ const filteredNumbers = filter(numbers, x => x < 10)
 console.log(filteredNumbers)
 
 // TEST 2 - skriv själv
-
+const numbers2 = [5, 10, 1, 15, 45, 67, 87, 35, 25, 18, 6, 8, 2]
+const filteredNumbers2 = numbers2.filter(function(x) {
+    return x > 20
+})
+console.log(filteredNumbers2)
 
 // TEST 3 - skriv själv
+const numbers3 = [1, 5, 4, 9, 8, 20, 10, 4, 14, 27, 5, 3, 9, 6, 7, 5]
+const filteredNumbers3 = numbers3.filter(function(x) {
+    return x === 5
+})
+console.log(filteredNumbers3)
